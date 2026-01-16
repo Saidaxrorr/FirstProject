7d01c2e36f25ef1e5032c4f32243c591b71be85c
axror <notmasterpoo1@gmail.com>
2026-01-16 22:47:50 +0500

    Code added

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace src
+﻿namespace src;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("1 son kiriting: ");
+        var number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("2 son ketring: ");
+        var number2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("(+, -, *, /): ");
+        string abc = Console.ReadLine(); // shotda (char abs = Console.ReadLine();) ishlatsa boladimi?
+
+        if (abc == "+")
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(number1 - number2);
+        }
+        else if (abc == "-")
+        {
+            Console.WriteLine(number1 - number2);
+        }
+        else if (abc == "*")
+        {
+            Console.WriteLine(number1 - number2);
+        }
+        else if (abc == "/")
+        {
+            Console.WriteLine(number1 - number2);
+        }
+        else
+        {
+            Console.WriteLine("Notog'ri amal!");
         }
     }
 }
